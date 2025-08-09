fdde2ede3b2280ab62eea2ca1352560b017bfe3b
matteo2302 <matteo.pizzichemi01@gmail.com>
2025-08-09 15:24:27 +0200

    end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import { useState, useEffect } from 'react';   
-import Meteoinfo from './MeteoInfo';  
+import { useState, useEffect } from 'react';
+import Meteoinfo from './MeteoInfo';
 
 import './App.css';
 
 function App() {
-let [meteo, setMeteo] = useState(null);
-let [caricamento, setCaricamento] = useState(true);
-let [errore,setErrore] =useState(null);
-useEffect(()=>{
-  const fecthMeteo = async () => {
-  try {
-    let res = await fetch('https://api.open-meteo.com/v1/forecast?latitude=44.1&longitude=8.27&current_weather=true');
-    let data = await res.json();
-    setMeteo(data.current_weather);
-    setCaricamento(false);
-  }catch(err){
-    setErrore("errore nel caricamento dei dati");
-    setCaricamento(false);
-  };
-}
-  fecthMeteo();
-}, []);
+  let [meteo, setMeteo] = useState(null);
+  let [caricamento, setCaricamento] = useState(true);
+  let [errore, setErrore] = useState(null);
+  useEffect(() => {
+    const fecthMeteo = async () => {
+      try {
+        let res = await fetch('https://api.open-meteo.com/v1/forecast?latitude=44.1&longitude=8.27&current_weather=true');
+        let data = await res.json();
+        setMeteo(data.current_weather);
+        setCaricamento(false);
+      } catch (err) {
+        setErrore("errore nel caricamento dei dati");
+        setCaricamento(false);
+      };
+    }
+    fecthMeteo();
+  }, []);
 
 
   return (
@@ -28,9 +28,9 @@ useEffect(()=>{
       <h1>Meteo Loano</h1>
       {caricamento && <p>Caricamento...</p>}
       {errore && <p>{errore}</p>}
-      {meteo &&  <Meteoinfo meteo={meteo} />}
+      {meteo && <Meteoinfo meteo={meteo} />}
 
-      
+
     </div>
   );
 }
