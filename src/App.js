7029984c3737ad6edf417dce0995d9b9b554dc19
matteo2302 <matteo.pizzichemi01@gmail.com>
2025-08-09 15:09:08 +0200

    call to the api

--- src/App.js
+++ src/App.js
@@ -6,10 +6,28 @@ function App() {
 let [meteo, setMeteo] = useState(null);
 let [caricamento, setCaricamento] = useState(true);
 let [errore,setErrore] =useState(null);
+useEffect(()=>{
+  const fecthMeteo = async () => {
+  try {
+    let res = await fetch('https://api.open-meteo.com/v1/forecast?latitude=44.1&longitude=8.27&current_weather=true');
+    let data = await res.json();
+    setMeteo(data.current_weather);
+    setCaricamento(false);
+  }catch(err){
+    setErrore("errore nel caricamento dei dati");
+    setCaricamento(false);
+  };
+}
+  fecthMeteo();
+}, []);
 
 
   return (
     <div className="App">
+      <h1>Meteo Loano</h1>
+      {caricamento && <p>Caricamento...</p>}
+      {errore && <p>{errore}</p>}
+      {meteo &&  <WeatherInfo meteo={meteo} />}
 
       
     </div>
