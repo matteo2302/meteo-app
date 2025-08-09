01eb645076ec66db879ccb229e20bdb7797874c7
matteo2302 <matteo.pizzichemi01@gmail.com>
2025-08-09 16:00:52 +0200

    ghour and minute included

--- src/MeteoInfo.js
+++ src/MeteoInfo.js
@@ -1,11 +1,12 @@
-function Meteoinfo({meteo}){
+function Meteoinfo({ meteo }) {
     return (
-        
-    <div>
-      <p>Temperatura: {meteo.temperature} °C</p>
-      <p>Velocità vento: {meteo.windspeed} km/h</p>
-      <p>Direzione vento: {meteo.winddirection}°</p>
-    </div>
+
+        <div>
+            <p>ora di rilevazione:{new Date(meteo.time).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}</p>
+            <p>Temperatura: {meteo.temperature} °C</p>
+            <p>Velocità vento: {meteo.windspeed} km/h</p>
+            <p>Direzione vento: {meteo.winddirection}°</p>
+        </div>
     )
 };
 export default Meteoinfo;
