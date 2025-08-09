001dae2edd608174718c7085447346334502f3a5
matteo2302 <matteo.pizzichemi01@gmail.com>
2025-08-09 14:40:30 +0200

    meteo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';   
+
 import './App.css';
 
 function App() {
+let [meteo, setMeteo] = useState(null);
+let [caricamento, setCaricamento] = useState(true);
+let [errore,setErrore] =useState(null);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      
     </div>
   );
 }
