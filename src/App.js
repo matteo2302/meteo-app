0e4f2bc62083338bbdaf90c7f34b38b7a5b175f0
matteo2302 <matteo.pizzichemi01@gmail.com>
2025-09-12 16:57:13 +0200

    added a dark mode

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 import Preferiti from "./pages/Preferiti";
 import DettagliMeteo from './pages/DettagliMeteo';
 import useMeteo from './hooks/useMeteo';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 import UsePreferiti from './hooks/UsePreferiti';
 import UseGeolocalizzazione from './hooks/UseGeolocalizzazione';
 import 'leaflet/dist/leaflet.css';
@@ -13,8 +13,11 @@ import './App.css';
 
 function App() {
   const [coordinate, setCoordinate] = useState({ latitude: null, longitude: null, nome: "" });
-
   const { meteo, caricamento, errore } = useMeteo(coordinate);
+  let [tema, setTema] = useState("light");
+  function toggleTema() {
+    setTema(prev => (prev === "light" ? "dark" : "light"));
+  }
 
   const { preferiti, aggiungiPreferito, rimuoviPreferito, isPreferito } = UsePreferiti();
   useEffect(() => {
@@ -22,43 +25,48 @@ function App() {
   }, []);
   return (
     <Router>
-      <Navbar bg="dark" data-bs-theme="dark" expand="lg">
-        <Container>
-          <Navbar.Brand href="#">🌤️ MeteoApp</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="/preferiti">Preferiti</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <div className={`App ${tema}`}>
+        <Navbar bg="dark" data-bs-theme="dark" expand="lg">
+          <Container>
+            <Navbar.Brand href="#">🌤️ MeteoApp</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/preferiti">Preferiti</Nav.Link>
+              </Nav>
+              <Button variant="secondary" onClick={toggleTema}>
+                Cambia tema
+              </Button>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
 
-      <Container className="mt-4">
-        <Routes>
-          <Route path="/" element={
-            <Home
-              meteo={meteo}
-              caricamento={caricamento}
-              errore={errore}
-              setCoordinate={setCoordinate}
-              aggiungiPreferito={aggiungiPreferito}
-              preferiti={preferiti}
-              rimuoviPreferito={rimuoviPreferito}
-              coordinate={coordinate}
-            />
-          } />
-          <Route path="/preferiti" element={
-            <Preferiti
-              preferiti={preferiti}
-              onSeleziona={(c) => setCoordinate(c)}
-              onRimuovi={rimuoviPreferito}
-            />
-          } />
-          <Route path="/DettagliMeteo/:nome" element={<DettagliMeteo />} />
-        </Routes>
-      </Container>
+        <Container className="mt-4">
+          <Routes>
+            <Route path="/" element={
+              <Home
+                meteo={meteo}
+                caricamento={caricamento}
+                errore={errore}
+                setCoordinate={setCoordinate}
+                aggiungiPreferito={aggiungiPreferito}
+                preferiti={preferiti}
+                rimuoviPreferito={rimuoviPreferito}
+                coordinate={coordinate}
+              />
+            } />
+            <Route path="/preferiti" element={
+              <Preferiti
+                preferiti={preferiti}
+                onSeleziona={(c) => setCoordinate(c)}
+                onRimuovi={rimuoviPreferito}
+              />
+            } />
+            <Route path="/DettagliMeteo/:nome" element={<DettagliMeteo />} />
+          </Routes>
+        </Container>
+      </div>
     </Router>
   );
 }
